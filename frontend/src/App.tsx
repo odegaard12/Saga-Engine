0f13308f13fd8e7386d9b8e8e4b5b982c529c7ec
odegaard12 <153871346+odegaard12@users.noreply.github.com>
2026-04-28 10:57:55 +0200

    Add React admin shell route (#68)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import AdminApp from './admin/AdminApp'
 import LoginApp from './login/LoginApp'
 import PlayerApp from './player/PlayerApp'
 import { getPlayerNameFromLocation } from './shared/playerRoute'
 
 export default function App() {
+  const path = window.location.pathname
+
+  if (path === '/admin-react' || path.startsWith('/admin-react/')) {
+    return <AdminApp />
+  }
+
   const user = getPlayerNameFromLocation()
   return user ? <PlayerApp /> : <LoginApp />
 }
