b74f6aa55098a01f093122324274d1b925a65440
odegaard12 <153871346+odegaard12@users.noreply.github.com>
2026-05-18 09:42:11 +0200

    feat: add global SAGA design tokens v1 + base reset

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import './styles/tokens.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles/mobile-shell.css'
-import './player/components/map-surface.css'
-import { setupLegacySpanishBridge } from './i18n/legacySpanishBridge'
-import { setupAdminSettingsLanguageMenu } from './i18n/adminSettingsLanguageMenu'
 
-
-setupLegacySpanishBridge()
-setupAdminSettingsLanguageMenu()
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 )
